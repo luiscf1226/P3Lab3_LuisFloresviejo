467fcbc53ebb55685f65384c3c341f2958a51997
unknown <luiscf1226@gmail.com>
2021-02-05 16:58:39 -0600

    push final

--- main.cpp
+++ main.cpp
@@ -3,6 +3,30 @@
 #include <algorithm>
 #include <string>
 using namespace std;
+int* ex3(int*point,int n){
+	int array[n];
+	int array2[n];
+	for(int i=0;i<n;i++){
+		array[i]=point[i];
+		cout<<array[i]<<endl;
+	}
+	int mayor=0;
+	for(int k=0;k<n;k++){
+		if(array[k]>mayor){
+			mayor=array[k];
+		}else{
+			mayor=mayor;
+		}
+		array2[k]=mayor;	
+	}
+	int array3[n];
+	for(int j=0;j<n;j++){
+		array2[j]=array3[j];
+	}
+	return array3;
+	
+	
+}
 void printvector(vector <char> nuevo){
 	  for(int k=0;k<nuevo.size();k++){
         	cout<<"["<<nuevo[k]<<"]";
@@ -27,29 +51,41 @@ vector <char> ex1 (string palabra){
         }
         return nuevo;
     }
-void ex2(string palabra, vector <char> vect){
+int*  ex2(string palabra, vector <char> vect){
+	
 	int num=0;
 	int num2=vect.size();
-	cout<<num2<<endl;
 	int array[num2];
 	string remp=" ";
 	int cont=0;
+	char letter;
+	string acum="";
 	//char array =[num2];
-
-	for(int i=0;i<palabra.length();i++){
-		for(int j=0;j<vect.size();j++){
-			if(palabra.at(i)==vect[j]){
-				cont++;
-				cout<<"si"<<endl;
-				cout<<cont<<endl;
-				int c=0;
-				array[0]+=cont;
-				c++;
+	for(int i=97;i<=122;i++){
+		for(int k=0;k<palabra.length();k++){
+		letter =palabra.at(k);
+			if(i==(int)letter){
+				acum+=letter;
 			}
 		}
 	}
-	cout<<array<<endl;
-	//return punt;
+
+	//cout<<acum<<endl;
+	for(int i=0;i<palabra.length();i++){
+		for(int j=0;j<vect.size();j++){
+			if(palabra.at(i)==acum[j]){
+				cont++;	
+			}
+		}
+	}
+	for(int g=0;g<num2;g++){
+		array[g]=cont;
+	
+	}
+	int* point= array;
+	return point;
+	delete point;
+	//cout<<cont<<endl;
 }  
 int main (){
 	int main=0;
@@ -75,11 +111,29 @@ int main (){
 			for(int i=0;i<word2.length();i++){
 				vect.push_back(word2.at(i));
 			}
-			//printvector(vect);
-			ex2(word,vect);
+		
+			cout<<"["<<*ex2(word,vect)<<"]";
+			//solo pude imprimir la cantidad de veces repoetida de todas
+			cout<<endl;
+
+
 		}
 		if(main==3){
-			
+			int n;
+			cout<<"Ingrese tamano arreglo deseado:";
+			cin>>n;
+			int *point;
+			point = new int[n];
+			for(int i=0;i<n;i++){
+				cout<<" Ingrese numero para rellanar: ";
+				cin>>point[i];
+			}
+			/*
+			for(int j=0;j<n;j++){
+				cout<<point[j]<<endl;
+			}*/
+			cout<<*ex3(point,n);
+			delete[]point;
 		}
 	}
 
