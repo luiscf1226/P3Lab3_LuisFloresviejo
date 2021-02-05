08cb61ba7bffa9b79ff6f1efe8b886d70920ec39
unknown <luiscf1226@gmail.com>
2021-02-05 13:37:32 -0600

    Primer creacion

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iostream >
+using namespace std;
+vector <int> ret1(string palabra){
+	
+}
+int main (){
+	int main=0;
+	while(main!=4) {
+		cout<<"1. Ejercisio 1 "<<endl<<"2. Ejercisio 2"<<endl<<"3. Ejercisio 3"<<endl<<"Ejercisio 4"<<endl<<"Escoja: ";
+		cin>>main;
+		if(main==1){
+			cout<<"1. Ejercisio 1..."<<endl;
+		}
+	}
+
+	return 0;
+}
